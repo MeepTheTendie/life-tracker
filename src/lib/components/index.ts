cc9f8c63d778249ad68afb8246edcffbfce6d7d5
Wesley Pennock <wpennock@syn-2603-8081-40f0-9260-0000-0000-0000-152a.res6.spectrum.com>
2026-02-06 20:03:54 -0600

    Reset main branch for rebuild - legacy code preserved in legacy branch

--- src/lib/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Dashboard } from "./Dashboard.svelte";
-export { default as HabitCard } from "./HabitCard.svelte";
-export { default as HabitForm } from "./HabitForm.svelte";
-export { default as ManageHabits } from "./ManageHabits.svelte";
-export { default as Analytics } from "./Analytics.svelte";
